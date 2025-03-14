8a4307a727090afa514f7de868ae7e4975501598
maybeags <suren0709@gmail.com>
2025-03-14 11:37:57 +0900

    feat: 20250314 nested if sentence

---
+++ src/main/java/ch06_condition/Condition06.java
@@ -0,0 +1,57 @@
+package ch06_condition;
+/*
+    중첩 if(Nested if condition sentence)
+        if 문 내에 if문이 연속적으로 작성될 수 있습니다.
+
+    형식 :
+    if(조건식1) {
+        실행문1
+        if(조건식1-a) {
+            실행문1-a
+        } else if(조건식1-b) {
+            실행문1-b
+        } else {
+            실행문1-c
+        }
+    } else if(조건식2) {
+        실행문2
+        if(조건식2-a) {
+            실행문2-a
+        } else {
+            실행문2-b
+        }
+    } else {
+        실행문3
+    }
+ */
+public class Condition06 {
+    public static void main(String[] args) {
+        /*
+            사용자에게 score를 입력 받아 다음과 같은 조건을 만족시키도록 작성하시오.
+
+            score가 0보다 작거나 100 초과라면 grade는 x
+            score 90 ~ 100, grade = A
+            score 80 ~ 90, grade = B
+            score 70 ~ 80, grade = C
+            score 60 ~ 70, grade = D
+            score 50 ~ 59, grade = F
+
+            실행 예 # 1
+            점수를 입력하세요 >>> -10
+            입력한 점수는 -10점이며 학점은 x학점입니다.
+
+            실행 예 # 2
+            점수를 입력하세요 >>> 92
+            입력한 점수는 92점이며 학점은 A학점입니다.
+         */
+
+        // 1. Scanner import
+
+        // 2. int score 선언(및 초기화)
+
+        // 3. String grade 선언(및 초기화)
+
+        // 4. Scanner를 입력 받기 위한 안내문 작성
+
+    }
+}
