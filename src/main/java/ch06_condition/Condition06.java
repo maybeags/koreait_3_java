90144db4f63494d073523a4da66fcf99b8f7966a
maybeags <suren0709@gmail.com>
2025-03-14 12:46:26 +0900

    feat: 20250314 switch sentence

--- src/main/java/ch06_condition/Condition06.java
+++ src/main/java/ch06_condition/Condition06.java
@@ -1,4 +1,7 @@
 package ch06_condition;
+
+import java.util.Scanner;
+
 /*
     중첩 if(Nested if condition sentence)
         if 문 내에 if문이 연속적으로 작성될 수 있습니다.
@@ -34,7 +37,7 @@ public class Condition06 {
             score 80 ~ 90, grade = B
             score 70 ~ 80, grade = C
             score 60 ~ 70, grade = D
-            score 50 ~ 59, grade = F
+            score 0 ~ 59, grade = F
 
             실행 예 # 1
             점수를 입력하세요 >>> -10
@@ -46,12 +49,56 @@ public class Condition06 {
          */
 
         // 1. Scanner import
+        Scanner scanner = new Scanner(System.in);
 
         // 2. int score 선언(및 초기화)
+        int score = 0;
 
         // 3. String grade 선언(및 초기화)
-
+        String grade = "";
         // 4. Scanner를 입력 받기 위한 안내문 작성
 
+        System.out.print("점수를 입력하세요 >>> ");
+        score = scanner.nextInt();
+
+        // 중첩 if 사용 사례
+//        if (score < 0) {
+//            grade = "x";
+//        } else {        // 얘의 조건은 grade >= 0이 됩니다.
+//            if (score > 100) {
+//                grade = "x";
+//            } else {        // 이 부분의 조건은 grade>=0 && grade <= 100 이 됩니다.
+//                if(score > 90) {
+//                    grade = "A";
+//                } else if (score > 80) {
+//                    grade = "B";
+//                } else if (score > 70) {
+//                    grade = "C";
+//                } else if (score >= 60) {
+//                    grade = "D";
+//                } else {    // 그럼 이제 여기의 조건은 score >= 0 && score < 60
+//                    grade = "F";
+//                }
+//            }
+//        }
+
+        // 논리 연산자를 사용한 if문
+
+        if (score > 100 || score < 0) { // 100 초과 및 0 미만을 거르는 조건문 작성
+            grade = "x";
+        } else {
+            if(score > 90) {
+                grade = "A";
+            } else if (score > 80) {
+                grade = "B";
+            } else if (score > 70) {
+                grade = "C";
+            } else if (score >= 60) {
+                grade = "D";
+            } else {
+                grade = "F";
+            }
+        }
+        System.out.println("점수는 " + score + "점이며, 학점은 " + grade + "등급입니다.");
     }
 }
